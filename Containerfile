73192e0211ef9bd1dc8fb4401284bce758fd52c4
Alex Garcia <lex@ibm.com>
2024-11-27 09:04:57 -0600

    Refactor build script

--- Containerfile
+++ Containerfile
@@ -1,9 +1,7 @@
-# FROM registry.access.redhat.com/ubi8/ubi:latest
 FROM quay.io/almalinuxorg/almalinux:8.10
 
 RUN dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm && \
     /usr/bin/crb enable && \
-    # dnf config-manager --enable "codeready-builder-for-rhel-8-$(arch)-rpms" && \
     dnf module enable -y nodejs:20 && \
     dnf install -y  clang \
                     clang-tools-extra \
@@ -72,21 +70,37 @@ RUN dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.n
                     gcc-toolset-13-libatomic-devel \
                     nodejs \
                     yarnpkg \
-                    ; \
-    pip-3.9 install httplib2 && \
+                    binutils-powerpc64le-linux-gnu \
+                    cpio \
+                    double-conversion-devel \
+                    flac-devel \
+                    highway-devel \
+                    lcms2-devel \
+                    libXNVCtrl-devel \
+                    libsecret-devel \
+                    libxslt-devel \
+                    opus-devel
+
+RUN pip-3.9 install httplib2 && \
     alternatives --set python /usr/bin/python3.9 && \
     alternatives --set python3 /usr/bin/python3.9
 
 RUN --mount=type=bind,source=patches/fix-depot-tools.patch,dst=/tmp/fix-depot-tools.patch \
+    --mount=type=bind,source=patches/fix-gn.patch,dst=/tmp/fix-gn.patch \
     git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git /opt/depot_tools && \
     cd /opt/depot_tools && \
-    patch -p1 -B .fix-depot-tools/ -i /tmp/fix-depot-tools.patch
-
-RUN --mount=type=bind,source=patches/fix-gn.patch,dst=/tmp/fix-gn.patch \
+    patch -p1 -B .fix-depot-tools/ -i /tmp/fix-depot-tools.patch && \
     git clone https://gn.googlesource.com/gn /opt/gn && \
     cd /opt/gn && \
     patch -p1 -B .fix-gn/ -i /tmp/fix-gn.patch && \
     python3 build/gen.py && \
     ninja -j $(nproc) -C out
+
+RUN cd / && \
+    ln -sf ./usr/bin && \
+    ln -sf ./usr/sbin && \
+    ln -sf ./usr/lib && \
+    ln -sf ./usr/lib64 && \
+    ln -sf ../../lib64/pkgconfig /usr/lib/pkgconfig
 
 ENV PATH="/opt/depot_tools:/opt/gn/out:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
--- build-electron.sh
+++ build-electron.sh
@@ -18,59 +18,30 @@
 #
 # ----------------------------------------------------------------------------
 
-ELECTRON_VERSION=${1:-v32.2.5}
+PACKAGE_NAME="electron"
+PACKAGE_VERSION=${1:-"v32.2.5"}
+PACKAGE_URL="https://github.com/electron/electron"
 
 set -eux
-CWD=$(pwd)
 
-# Prepare for build
-export DEPOT_TOOLS_UPDATE=0
-export VPYTHON_BYPASS="manually managed python not supported by chrome operations"
-export GIT_CACHE_PATH="${CWD}"/.git_cache
-
-mkdir -p "${GIT_CACHE_PATH}"
-if [ -z "$(ls -A "${CWD}"/electron/src)" ]; then
-  # Checkout source
-  mkdir -p electron && cd electron
-  gclient config --name src/electron --unmanaged "https://github.com/electron/electron@${ELECTRON_VERSION}"
-  gclient sync --with_branch_heads --with_tags -vv
-  cd src
-
-  # Timothy Pearson's patchset
-  # https://gitlab.solidsilicon.io/public-development/open-source/chromium/openpower-patches/-/tree/chromium-128/patches/ppc64le
-  # Note(lex-ibm): We could automate getting the patches from the above URL, but after some discussions we decided it is better
-  # to have the patches in the same repository for better control/visibility.
-  while IFS= read -r patch; do
-    if [[ $patch =~ ^ppc64le ]]; then
-      git apply "${CWD}/patches/${patch}"
-    fi
-  done <"${CWD}"/patches/series
-
-  # EPEL8 Chromium patches
-  # https://src.fedoraproject.org/rpms/chromium
-  git apply "${CWD}"/patches/fedora/chromium-118-dma_buf_export_sync_file-conflict.patch
-  git apply "${CWD}"/patches/fedora/chromium-127-rust-clanglib.patch
-
-  # Electron PowerPC64 Little Endian support
-  git apply "${CWD}"/patches/electron-32-fix-runtime-api-delegate.patch
-  git apply "${CWD}"/patches/electron-32-fix-ppc64-syscalls-headers.patch
-
-  # Use RHEL's libpng
-  build/linux/unbundle/replace_gn_files.py --system-libraries libpng
-
-  cd ../../
-fi
-
-# Build
-CXXFLAGS+=' -faltivec-src-compat=mixed -Wno-deprecated-altivec-src-compat'
-
+# Export variables
 export CC=clang
 export CXX=clang++
 export AR=llvm-ar
 export NM=llvm-nm
 export READELF=llvm-readelf
 
+build_dir="${BUILD_DIRECTORY:-"$PWD/build"}"
+patches_dir="$PWD/patches"
+electron_src="${build_dir}/src"
+electron_out="${electron_src}/out/Release"
+
+CXXFLAGS+=' -faltivec-src-compat=mixed -Wno-deprecated-altivec-src-compat'
 export CXXFLAGS
+
+export DEPOT_TOOLS_UPDATE=0
+export VPYTHON_BYPASS="manually managed python not supported by chrome operations"
+export GIT_CACHE_PATH="${GIT_CACHE_PATH:-${PWD}/.git_cache}"
 
 # need for error: the option `Z` is only accepted on the nightly compiler
 export RUSTC_BOOTSTRAP=1
@@ -97,17 +68,74 @@ ELECTRON_GN_DEFINES+=' rust_sysroot_absolute="/usr"'
 ELECTRON_GN_DEFINES+=" rust_bindgen_root=\"$rust_bindgen_root\""
 ELECTRON_GN_DEFINES+=" rustc_version=\"$rustc_version\""
 
-ELECTRON_GN_DEFINES+=' use_system_libffi=true'
+ELECTRON_GN_DEFINES+=' rtc_use_pipewire=false rtc_link_pipewire=false'
 
 ELECTRON_GN_DEFINES+=' target_cpu="ppc64"'
-ELECTRON_GN_DEFINES+=' target_os="linux"'
-ELECTRON_GN_DEFINES+=' current_os="linux"'
+if [ "$(arch)" != "ppc64le" ]; then
+  ELECTRON_GN_DEFINES+=' use_sysroot=true'
+fi
+
 ELECTRON_GN_DEFINES+=' treat_warnings_as_errors=false'
 ELECTRON_GN_DEFINES+=' use_gnome_keyring=false'
+ELECTRON_GN_DEFINES+=' use_system_libffi=true'
 
-# ELECTRON_GN_DEFINES+=' use_sysroot=true'
+# Create git cache directory if not already present
+if [ ! -d "${GIT_CACHE_PATH}" ]; then
+  mkdir -p "${GIT_CACHE_PATH}"
+fi
 
-cd electron/src
+# Ensure that compiler-rt and rustlib are there
+if [ ! -d /usr/lib/clang/18/lib/ppc64le-redhat-linux-gnu/ ] && [ "$(arch)" != "ppc64le" ]; then
+  ln -s /sysroot/usr/lib/clang/18/lib/ppc64le-redhat-linux-gnu /usr/lib/clang/18/lib/ppc64le-redhat-linux-gnu
+fi
+if [ ! -d /usr/lib/rustlib/powerpc64le-unknown-linux-gnu/ ] && [ "$(arch)" != "ppc64le" ]; then
+  ln -s /sysroot/usr/lib/rustlib/powerpc64le-unknown-linux-gnu /usr/lib/rustlib/powerpc64le-unknown-linux-gnu
+fi
+
+# Checkout source
+
+if [ ! -d "${build_dir}" ]; then
+  mkdir -p "${build_dir}"
+fi
+cd "${build_dir}"
+
+gclient config --name src/electron --unmanaged "${PACKAGE_URL}@${PACKAGE_VERSION}"
+gclient sync --with_branch_heads --with_tags -vv
+
+cd "${electron_src}"
+
+# Don't use debian sysroots, even when cross-compiling
+rm -rf build/linux/debian_bullseye_*
+ln -s / build/linux/debian_bullseye_amd64-sysroot
+ln -s / build/linux/debian_bullseye_arm64-sysroot
+if [ "$(arch)" == "ppc64le" ]; then
+  ln -s / build/linux/debian_bullseye_ppc64el-sysroot
+else
+  ln -s /sysroot build/linux/debian_bullseye_ppc64el-sysroot
+fi
+
+# Timothy Pearson's patchset
+# https://gitlab.solidsilicon.io/public-development/open-source/chromium/openpower-patches/-/tree/chromium-128/patches/ppc64le
+# Note(lex-ibm): We could automate getting the patches from the above URL, but after some discussions we decided it is better
+# to have the patches in the same repository for better control/visibility.
+while IFS= read -r patch; do
+  if [[ $patch =~ ^ppc64le ]]; then
+    git apply "${patches_dir}/${patch}"
+  fi
+done <"${patches_dir}"/series
+
+# EPEL8 Chromium patches
+# https://src.fedoraproject.org/rpms/chromium
+git apply "${patches_dir}"/fedora/chromium-118-dma_buf_export_sync_file-conflict.patch
+git apply "${patches_dir}"/fedora/chromium-127-rust-clanglib.patch
+
+# Electron PowerPC64 Little Endian support
+git apply "${patches_dir}"/electron-32-001-fix-runtime-api-delegate.patch
+git apply "${patches_dir}"/electron-32-002-fix-ppc64-syscalls-headers.patch
+git apply "${patches_dir}"/electron-32-003-enable-ppc64le-cross-compile.patch
+
+# Build
+cd "${electron_src}"
 
 cp "$(command -v node)" third_party/node/linux/node-linux-x64/bin/node
 chmod +x third_party/node/linux/node-linux-x64/bin/node
@@ -115,22 +143,22 @@ chmod +x third_party/node/linux/node-linux-x64/bin/node
 rm -rf buildtools/third_party/eu-strip/bin/eu-strip
 cp "$(command -v eu-strip)" buildtools/third_party/eu-strip/bin/eu-strip
 
-if [ ! -f "${CWD}"/electron/src/out/Release/electron ]; then
-  gn gen out/Release --args="import(\"//electron/build/args/release.gn\") ${ELECTRON_GN_DEFINES}"
-  ninja -j "$(nproc)" -C out/Release electron
-  electron/script/strip-binaries.py -d out/Release
-  ninja -C out/Release electron:electron_dist_zip
+if [ ! -f "${electron_out}/electron" ]; then
+  gn gen "${electron_out}" --args="import(\"//electron/build/args/release.gn\") ${ELECTRON_GN_DEFINES}"
+  ninja -j "$(nproc)" -C "${electron_out}" electron
+  electron/script/strip-binaries.py --target-cpu ppc64 -d "${electron_out}"
+  ninja -C "${electron_out}" electron:electron_dist_zip
 fi
 
-if [ -f "${CWD}"/electron/src/out/Release/dist.zip ]; then
-  mv "${CWD}"/electron/src/out/Release/dist.zip "${CWD}/electron/src/out/Release/electron-${ELECTRON_VERSION}-linux-ppc64le.zip"
+if [ -f "${electron_out}/dist.zip" ]; then
+  mv "${electron_out}/dist.zip" "${electron_out}/${PACKAGE_NAME}-${PACKAGE_VERSION}-linux-ppc64le.zip"
 fi
 
-if [ -f "${CWD}/electron/src/out/Release/electron-${ELECTRON_VERSION}-linux-ppc64le.zip" ]; then
-  shasum256_zip=$(sha256sum "${CWD}/electron/src/out/Release/electron-${ELECTRON_VERSION}-linux-ppc64le.zip")
-  if [ ! -f "${CWD}"/electron/src/out/Release/SHASUM256.txt ]; then
-    curl -sL "https://github.com/electron/electron/releases/download/${ELECTRON_VERSION}/SHASUMS256.txt" >"${CWD}"/electron/src/out/Release/SHASUM256.txt
-    printf '\n%s' "$shasum256_zip" >>"${CWD}"/electron/src/out/Release/SHASUM256.txt
+if [ -f "${electron_out}/${PACKAGE_NAME}-${PACKAGE_VERSION}-linux-ppc64le.zip" ]; then
+  shasum256_zip=$(sha256sum "${electron_out}/${PACKAGE_NAME}-${PACKAGE_VERSION}-linux-ppc64le.zip")
+  if [ ! -f "${electron_out}/SHASUM256.txt" ]; then
+    curl -sL "https://github.com/electron/electron/releases/download/${PACKAGE_VERSION}/SHASUMS256.txt" > "${electron_out}/SHASUM256.txt"
+    printf '\n%s' "$shasum256_zip" >> "${electron_out}/SHASUM256.txt"
   fi
   echo "$shasum256_zip"
 fi
