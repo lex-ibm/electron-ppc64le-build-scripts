5f246d06dcfff5f7688368317fbbf7128ec09f9e
Alex Garcia <lex@ibm.com>
2024-11-29 19:10:52 -0600

    Electron v32.2.6

--- build-electron.sh
+++ build-electron.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package         : Electron
-# Version         : 32.2.5
+# Version         : 32.2.6
 # Source repo     : https://github.com/electron/electron
 # Tested on       : RHEL 8.10
 # Language        : C++
@@ -20,7 +20,7 @@
 
 # shellcheck disable=SC2034
 PACKAGE_NAME="electron"
-PACKAGE_VERSION=${1:-"v32.2.5"}
+PACKAGE_VERSION=${1:-"v32.2.6"}
 PACKAGE_URL="https://github.com/electron/electron"
 
 set -eux
