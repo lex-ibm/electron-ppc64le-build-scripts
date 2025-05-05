d5b3d0232f1cde9e2a186a184173c9abd8898f37
Alex Garcia <lex@ibm.com>
2025-05-05 16:00:24 -0500

    Electron v34.5.4

--- build-electron.sh
+++ build-electron.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package         : Electron
-# Version         : 34.2.0
+# Version         : 34.5.4
 # Source repo     : https://github.com/electron/electron
 # Tested on       : RHEL 8.10
 # Language        : C++
@@ -20,7 +20,7 @@
 
 # shellcheck disable=SC2034
 PACKAGE_NAME="electron"
-PACKAGE_VERSION=${1:-"v34.2.0"}
+PACKAGE_VERSION=${1:-"v34.5.4"}
 PACKAGE_URL="https://github.com/electron/electron"
 
 set -eux
